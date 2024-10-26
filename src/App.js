baaf310dc87a72c23f01108c6d18f6a615be0726
sudhakargithub321 <sudhakarbg0508@gmail.com>
2024-10-27 00:28:10 +0530

    Added all files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Homepage from './Homepage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+     <Homepage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/timeline/Sugesstions.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import "./Sugesstions.css"
+import { Avatar } from '@mui/material'
+function Sugesstions() {
+  return (
+    <div className="Sugesstions">
+      <div className="sugessions__title">Sugesstions for you</div>
+      <div className="sugessions__usernames">
+
+      <div className="sugessions__username">
+        <div className="username__left">
+          <span className="avatar">
+            <Avatar>R</Avatar>
+          </span>
+          <div className="username__info">
+              <span className="username">redian_</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow</button>
+        </div>
+
+        <div className="sugessions__username">
+        <div className="username__left">
+          <span className="avatar">
+            <Avatar>R</Avatar>
+          </span>
+          <div className="username__info">
+              <span className="username">redian_</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow</button>
+        </div>
+
+        <div className="sugessions__username">
+        <div className="username__left">
+          <span className="avatar">
+            <Avatar>R</Avatar>
+          </span>
+          <div className="username__info">
+              <span className="username">redian_</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow</button>
+        </div>
+
+        <div className="sugessions__username">
+        <div className="username__left">
+          <span className="avatar">
+            <Avatar>R</Avatar>
+          </span>
+          <div className="username__info">
+              <span className="username">redian_</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow</button>
+        </div>
+
+        <div className="sugessions__username">
+        <div className="username__left">
+          <span className="avatar">
+            <Avatar>R</Avatar>
+          </span>
+          <div className="username__info">
+              <span className="username">redian_</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow</button>
+        </div>
+        </div>
+      </div>
+  )
+}
+
+export default Sugesstions
